45dd249453b5bce4f85e36a04fd9e7d8b7743fe3
Rafk123 <tabunanov.rr@phystech.edu>
2026-03-09 01:00:14 +0300

    tasks_2

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,45 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+
+    if pad_size < 1:
+        raise ValueError
+
+    if image.ndim == 2:
+        padded = np.zeros(np.array(image.shape) + 2 * pad_size, dtype=np.uint8)
+
+    if image.ndim == 3:
+        padded = np.zeros(
+            (image.shape[0] + 2 * pad_size, image.shape[1] + 2 * pad_size, image.shape[2]),
+            dtype=np.uint8,
+        )
+
+    padded[pad_size : padded.shape[0] - pad_size, pad_size : padded.shape[1] - pad_size] = image
+
+    return padded
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+
+    padded = pad_image(image, kernel_size // 2 + 1)
+    column_cumsum = np.cumsum(padded, axis=0, dtype=np.uint64)
+    block_column_sum = (
+        column_cumsum[kernel_size : column_cumsum.shape[0] - 1]
+        - column_cumsum[: column_cumsum.shape[0] - kernel_size - 1]
+    )
+    row_cumsum = np.cumsum(block_column_sum, axis=1, dtype=np.uint64)
+    row_cumsum[:, kernel_size : row_cumsum.shape[1] - 1] -= row_cumsum[
+        :, : row_cumsum.shape[1] - kernel_size - 1
+    ]
+    blur = row_cumsum[:, kernel_size : row_cumsum.shape[1] - 1] // kernel_size**2
+
+    return np.array(blur, dtype=np.uint8)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -1,10 +1,28 @@
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 
 
 def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
 
-    return 0, 0
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
+
+    threshold -= 1
+    cnt = np.bincount(image.getfield(np.uint8).ravel(), minlength=256)
+    ans = np.zeros(2 * (threshold + 1) + cnt.shape[0])
+    ans[threshold + 1 : ans.shape[0] - threshold - 1] = cnt
+    ans = np.cumsum(ans)
+    ans = (
+        ans[2 * threshold + 1 : ans.shape[0] - 1] - ans[: ans.shape[0] - 1 - 2 * threshold - 1]
+    ) * (cnt != 0)
+
+    index = np.arange(256, dtype=np.uint8)
+    mx = np.max(ans)
+    ans1 = (mx == ans) * index + (mx != ans) * -1
+    ans2 = np.max(ans * (ans1 != -1)) / np.sum(cnt)
+    ans1 = ans1[ans1 != -1]
+
+    return ans1[0].astype(np.uint8), ans2
