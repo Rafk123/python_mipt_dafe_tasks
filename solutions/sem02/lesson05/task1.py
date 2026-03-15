67239c2aad9cc1fd1c89500159754497faf81075
Rafk123 <tabunanov.rr@phystech.edu>
2026-03-16 00:58:00 +0300

    lesson05

--- solutions/sem02/lesson05/task1.py
+++ solutions/sem02/lesson05/task1.py
@@ -9,4 +9,18 @@ def can_satisfy_demand(
     costs: np.ndarray,
     resource_amounts: np.ndarray,
     demand_expected: np.ndarray,
-) -> bool: ...
+) -> bool:
+    
+    n = costs.shape[1]
+    m = costs.shape[0]
+    if n != demand_expected.size or m != resource_amounts.size:
+        raise ShapeMismatchError
+    
+    demand_expected = demand_expected[np.newaxis, :]
+    costs *= demand_expected
+    costs = np.sum(costs, axis=1, dtype=np.int64)
+
+    resource_amounts -= costs
+    resource_amounts = (resource_amounts < 0)
+
+    return (np.sum(resource_amounts) == 0)
--- solutions/sem02/lesson05/task3.py
+++ solutions/sem02/lesson05/task3.py
@@ -9,4 +9,13 @@ def adaptive_filter(
     Vs: np.ndarray,
     Vj: np.ndarray,
     diag_A: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+
+    m = Vs.shape[0]
+    k = Vj.shape[1]
+
+    if (m != Vj.shape[0] or k != diag_A.size):
+        raise ShapeMismatchError
+    
+    Vjh = Vj.T.conj()
+    return Vs - Vj @ np.linalg.inv(np.diag(np.ones(k)) + (Vjh @ Vj) * diag_A) @ (Vjh @ Vs)
