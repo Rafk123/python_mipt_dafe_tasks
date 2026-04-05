a13749792748932e0ff0a145d487a908421bc663
Rafk123 <tabunanov.rr@phystech.edu>
2026-04-06 01:10:47 +0300

    les7

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -1,21 +1,105 @@
-from typing import Any
+from typing import Any, Sequence
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.style.use('dark_background')
 
 class ShapeMismatchError(Exception):
     pass
-
 
 def visualize_diagrams(
     abscissa: np.ndarray,
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.size != ordinates.size:
+        raise ShapeMismatchError
+    
+    figure = plt.figure(figsize=(8, 8))
+    gs = plt.GridSpec(2, 2, width_ratios=[1, 3], height_ratios=[3, 1])
+    ord_ax = figure.add_subplot(gs[0, 0])
+    abs_ax = figure.add_subplot(gs[1, 1])
+    scat_ax = figure.add_subplot(gs[0, 1])
 
+    match diagram_type:
+        case 'hist':
+            ord_ax.hist(
+                ordinates,
+                bins = 50,
+                color='#0090B4',
+                edgecolor='#86CFFF',
+                density=True,
+                label='ordinates',
+                alpha=0.3,
+                orientation='horizontal',
+            )
+            abs_ax.hist(
+                abscissa,
+                bins=50,
+                color='#0090B4',
+                edgecolor='#86CFFF',
+                density=True,
+                label='abscissa',
+                alpha=0.3,
+                orientation='vertical',
+            )
+
+            ord_ax.invert_xaxis()
+            abs_ax.invert_yaxis()
+            ord_ax.legend(loc='lower left')
+            abs_ax.legend(loc='lower right')
+
+        case 'violin':
+            def set_colors(violin_parts) -> None:
+                for body in violin_parts['bodies']:
+                    body.set_facecolor('#0090B4')
+                    body.set_edgecolor('#86CFFF')
+
+                for part in violin_parts:
+                    if part != "bodies":
+                        violin_parts[part].set_edgecolor('#86CFFF')
+            
+            violin_parts = ord_ax.violinplot(
+                ordinates,
+                vert=True,
+                showmedians=True,
+            )
+            set_colors(violin_parts)
+
+            violin_parts = abs_ax.violinplot(
+                abscissa,
+                vert=False,
+                showmedians=True,
+            )
+            set_colors(violin_parts)
+
+        case 'box':
+            ord_ax.boxplot(
+                ordinates,
+                vert=True,
+                patch_artist=True,
+                boxprops=dict(facecolor='#0090B4', alpha=0.3),
+                medianprops=dict(color='#86CFFF'),
+            )
+            abs_ax.boxplot(
+                abscissa,
+                vert=False,
+                patch_artist=True,
+                boxprops=dict(facecolor='#0090B4', alpha=0.3),
+                medianprops=dict(color='#86CFFF'),
+            )
+
+        case _:
+            raise ValueError
+        
+    scat_ax.scatter(
+        abscissa, 
+        ordinates,
+        color='#0090B4',
+        edgecolors='#86CFFF',
+        alpha=0.3
+    )
 
 if __name__ == "__main__":
     mean = [2, 3]
@@ -25,4 +109,6 @@ if __name__ == "__main__":
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
 
     visualize_diagrams(abscissa, ordinates, "hist")
+    visualize_diagrams(abscissa, ordinates, "violin")
+    visualize_diagrams(abscissa, ordinates, "box")
     plt.show()
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,63 @@
 # ваш код (используйте функции или классы для решения данной задачи)
+
+from typing import Any, Sequence
+
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+plt.style.use('ggplot')
+
+def build_diagrams(data: dict[str:list[str]]) -> None:
+    figure = plt.figure(figsize=(20, 10))
+    axis = figure.add_subplot(1, 1, 1)
+
+    counts_before = np.array([0] * 4)
+    for i in range(len(data['before'])):
+        counts_before[0] += (data['before'][i] == 'I')
+        counts_before[1] += (data['before'][i] == 'II')
+        counts_before[2] += (data['before'][i] == 'III')
+        counts_before[3] += (data['before'][i] == 'IV')
+
+    counts_after = np.array([0] * 4)
+    for i in range(len(data['after'])):
+        counts_after[0] += (data['after'][i] == 'I')
+        counts_after[1] += (data['after'][i] == 'II')
+        counts_after[2] += (data['after'][i] == 'III')
+        counts_after[3] += (data['after'][i] == 'IV')
+
+    axis.set_title("Mitral disease stages", fontsize=17, fontweight="bold", c="#86CFFF")
+    axis.set_ylabel("amount of people", fontsize=14, fontweight="bold", c="#86CFFF")
+    axis.set_xticks(
+        np.arange(4),
+        labels=['I', 'II', 'III', 'IV'],
+        weight="bold",
+        fontsize=17,
+    )
+
+    axis.bar(
+        np.arange(4) - 0.15, 
+        np.array(counts_before),
+        label='before',
+        color='#0090B4',
+        edgecolor='#86CFFF',
+        width=0.3,
+    )
+    
+    axis.bar(
+        np.arange(4) + 0.15, 
+        np.array(counts_after),
+        label='after',
+        color="#B40000",
+        edgecolor="#FF8686",
+        width=0.3,
+    )
+
+    axis.legend(fontsize=17, labelcolor="#86CFFF")
+
+if __name__ == "__main__":
+    with open("medic_data.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    build_diagrams(data)
+    plt.show()
